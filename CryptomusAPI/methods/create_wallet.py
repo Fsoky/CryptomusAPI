24c1bafbdac88ce043c50e05b359db95e111f08b
Fsoky <cyberuest0x12@gmail.com>
2024-02-09 15:10:52 +0600

    Create wallet

--- CryptomusAPI/methods/create_wallet.py
+++ CryptomusAPI/methods/create_wallet.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 
 from .base import Method
-from ..types import WalletResponse
+from ..types import WalletResponse, BlockWalletResponse, RefundBlockedResponse
 
 
 @dataclass
 class CreateWallet(Method[WalletResponse]):
     __returning__ = WalletResponse
-    __api_method__ = "v1/payment/creating-static"
+    __api_method__ = "v1/wallet/creating-static"
     __http_method__ = "post"
 
     currency: str
@@ -15,3 +15,25 @@ class CreateWallet(Method[WalletResponse]):
     order_id: str
     url_callback: str | None = None
     from_referral_code: str | None = None
+
+
+@dataclass
+class BlockWallet(Method[BlockWalletResponse]):
+    __returning__ = WalletResponse
+    __api_method__ = "v1/wallet/block-address"
+    __http_method__ = "post"
+
+    uuid: str
+    order_id: str
+    is_force_refund: bool = False
+
+
+@dataclass
+class RefundBlocked(Method[RefundBlockedResponse]):
+    __returning__ = RefundBlockedResponse
+    __api_method__ = "v1/wallet/blocked-address-refund"
+    __http_method__ = "post"
+
+    address: str
+    uuid: str | None = None
+    order_id: str | None = None
