9ef9db3c79961aa5dad7d8b8710c2baa548151d2
Fsoky <cyberuest0x12@gmail.com>
2024-02-09 15:07:05 +0600

    Init file

--- CryptomusAPI/methods/__init__.py
+++ CryptomusAPI/methods/__init__.py
@@ -1,3 +1,15 @@
 from .get_payment_info import GetPaymentInfo
 from .create_invoice import CreateInvoice
-from .create_wallet import CreateWallet
+from .create_wallet import CreateWallet, BlockWallet
+from .generate_qr import GenerateQRWallet, GenerateQRMerchant
+from .refund import Refund
+
+__all__ = [
+    "GetPaymentInfo",
+    "CreateInvoice",
+    "CreateWallet",
+    "BlockWallet",
+    "GenerateQRWallet",
+    "GenerateQRMerchant",
+    "Refund"
+]
--- CryptomusAPI/types/__init__.py
+++ CryptomusAPI/types/__init__.py
@@ -1,2 +1,13 @@
 from .payment_info_response import PaymentInfoResponse
-from .wallet_response import WalletResponse
+from .wallet_response import WalletResponse, BlockWalletResponse, RefundBlockedResponse
+from .qr_response import QRCodeResponse
+from .refund_response import RefundResponse
+
+__all__ = [
+    "PaymentInfoResponse",
+    "WalletResponse",
+    "BlockWalletResponse",
+    "RefundBlockedResponse",
+    "QRCodeResponse",
+    "RefundResponse"
+]
