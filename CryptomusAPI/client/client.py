c8a656a29959e72e6fc9741b0aa8ed8b9accac9e
Fsoky <cyberuest0x12@gmail.com>
2024-02-09 15:06:55 +0600

    new methods

--- CryptomusAPI/client/client.py
+++ CryptomusAPI/client/client.py
@@ -1,11 +1,10 @@
-from decimal import Decimal
-from typing import Any
+from typing import Any, Literal
 
 from .session.base import BaseSession
 from .session._aiohttp import AIOHTTPSession
 
-from CryptomusAPI.methods import GetPaymentInfo, CreateInvoice, CreateWallet
-from CryptomusAPI.types import PaymentInfoResponse, WalletResponse
+from CryptomusAPI.methods import *
+from CryptomusAPI.types import *
 
 
 class CryptomusClient:
@@ -31,7 +30,7 @@ class CryptomusClient:
     async def get_payment_info(
         self, uuid: str, order_id: str | None = None
     ) -> PaymentInfoResponse:
-        return await self.session(GetPaymentInfo(**self.__get_func_params(locals())))
+        return await self.session(GetPaymentInfo(uuid, order_id))
     
     async def create_invoice(
         self,
@@ -71,3 +70,24 @@ class CryptomusClient:
         from_referral_code: str | None = None
     ) -> WalletResponse:
         return await self.session(CreateWallet(**self.__get_func_params(locals())))
+    
+    async def block_wallet(
+        self, uuid: str, order_id: str, is_force_refund: bool = False
+    ) -> BlockWalletResponse:
+        return await self.session(BlockWallet(uuid, order_id, is_force_refund))
+    
+    async def generate_qr(
+        self, uuid: str, for_what: Literal["wallet", "merchant"] = "wallet"
+    ) -> QRCodeResponse:
+        request = GenerateQRWallet(uuid) if for_what == "wallet" else GenerateQRMerchant(uuid)
+        return await self.session(request)
+    
+    async def blocked_address_refund(
+        self, address: str, *, uuid: str | None = None, order_id: str | None = None
+    ) -> RefundBlockedResponse:
+        return await self.session(RefundBlockedResponse(address, uuid, order_id))
+    
+    async def refund(
+        self, address: str, is_subtract: bool, uuid: str | None = None, order_id: str | None = None
+    ) -> RefundResponse:
+        return await self.session(Refund(address, is_subtract, uuid, order_id))
